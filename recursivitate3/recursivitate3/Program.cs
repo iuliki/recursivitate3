e992151c7e62c37382b68a510c06e5112e2c77d4
iuliki <iulia.bulugu@emanuel.ro>
2022-01-13 16:23:31 +0200

    rezolvare

---
+++ recursivitate3/recursivitate3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace recursivitate3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(fibonacci.fib(0, 1, 1, 7));
+        }
+    }
+}
---
+++ recursivitate3/recursivitate3/fibonacci.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace recursivitate3
+{
+    class fibonacci
+    {
+        public static int fib(int a,int b,int n, int p )
+
+        {
+             if (n == p)
+                return a;
+            else
+                return fib(b, a + b, n + 1, p);
+        }
+    }
+}
